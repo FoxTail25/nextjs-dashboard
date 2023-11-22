10a3ce87d77ba64c62c23943dc59842a79161bdf
Grigoriy <foxtail25@gmail.com>
2023-11-23 00:45:43 +0300

    clsx

---
+++ app/dashboard/customers/page.tsx
@@ -0,0 +1,12 @@
+import { Metadata } from 'next'
+ 
+export const metadata: Metadata = {
+  title: 'customers',
+}
+
+
+export default function Customers() {
+  return <>
+    <p>Customers page2</p>
+  </>
+}
---
+++ app/dashboard/invoices/page.tsx
@@ -0,0 +1,10 @@
+import { Metadata } from 'next'
+ 
+export const metadata: Metadata = {
+  title: 'invoices',
+}
+
+
+export default function Invoices() {
+    return <p>Invoices Page3</p>
+}
