b2f4e48ea6f28b07d0f2348ae95a18c6264900dc
Grigoriy <foxtail25@gmail.com>
2023-11-29 10:22:51 +0300

    db

--- app/api/base.js
+++ app/api/base.js
@@ -1,23 +1,9 @@
-import { NextApiRequest, NextApiResponse } from 'next'
-import {  ToDo } from '../../lib/todos'
-import * as todos from '../../lib/todos'
+import pg from 'pg';
 
-export default async function handler(
-  req: NextApiRequest,
-  res: NextApiResponse
-) {
-  switch (req.method) {
-    case 'GET':
-      return res.status(200).json(await todos.list())
-    case 'POST':
-      return res.status(201).json(await todos.create(req.body))
-    case 'PUT':
-      const updated = await todos.update(req.body)
-      return res.status(updated.length > 0 ? 200 : 404).json(updated)
-    case 'DELETE':
-      const removed = await todos.remove(req.body)
-      return res.status(removed.length > 0 ? 204 : 404).end()
-    default:
-      return res.status(405).send('Method Not Allowed')
-  }
-}
+const pool = new pg.Pool({
+  user: postgres,
+  password: "root",
+  host:"localhost",
+  port: 5432,
+  database: "node_posgre"
+})
